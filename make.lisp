c7ae0837fb723303e7801e712a1fad43c76ec9f7
John Mercouris <john@mercouris.email>
2018-01-29 00:59:42 +0100

    make script

--- make.lisp
+++ make.lisp
@@ -1,6 +1,10 @@
 (in-package :site-generator)
 
-(generate-site (get-path))
+(uiop:run-program "rm .database" :ignore-error-status t) ; clear database
+
+(let* ((current-directory (uiop:getcwd)))
+  (generate-site current-directory))
+
 (uiop:run-program "rm site/static") ; remove symlink
 (uiop:run-program "cp -r ./static site/static") ; copy files over
 
