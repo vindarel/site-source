0a9601b88d228546d6ece3f1110df82c05f98478
John Mercouris <john@mercouris.email>
2018-01-29 01:05:05 +0100

    make script exports to "export" folder

--- make.lisp
+++ make.lisp
@@ -2,9 +2,12 @@
 
 (uiop:run-program "rm .database" :ignore-error-status t) ; clear database
 
-(let* ((current-directory (uiop:getcwd)))
+(let* ((current-directory (uiop:getcwd))
+       (output-directory (merge-pathnames "export/" current-directory)))
+  (setf *site-dir* output-directory)
+  (print *site-dir*)
   (generate-site current-directory))
 
-(uiop:run-program "rm site/static") ; remove symlink
-(uiop:run-program "cp -r ./static site/static") ; copy files over
+(uiop:run-program "rm export/static" :ignore-error-status t) ; remove symlink
+(uiop:run-program "cp -r ./static export/static") ; copy files over
 
